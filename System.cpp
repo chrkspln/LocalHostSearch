92e04e7a59791151ff1279b98bd597585e0a6b18
Polina Cherkasova <polina_cherkasova@outlook.com>
2024-02-18 17:31:14 +0200

    added comments for code clarity

--- System.cpp
+++ System.cpp
@@ -1,6 +1,7 @@
 ﻿#include "System.h"
 #include <iostream>
 
+// тут я визначаю чи в юзера вінда чи лінукс аби правильно почати пошук
 #ifdef _WIN64
 #include <filesystem>
 std::filesystem::directory_entry System::starting_point{ "C:\\" };
@@ -12,17 +13,30 @@ std::filesystem::directory_entry System::starting_point{ "C:\\" };
 std::filesystem::directory_entry System::starting_point{ "/" };
 #endif
 
+// функція пошуку
 std::filesystem::path System::find_file_path(const std::string& file_name)
 {
-	for (const auto& entry : std::filesystem::recursive_directory_iterator(System::starting_point, std::filesystem::directory_options::skip_permission_denied)) {
+	// тут я через фор пробігаюсь по n-нній кількості directory_entry
+	// за виключенням тих директорій які потребують права адміна
+	for (const auto& entry : std::filesystem::recursive_directory_iterator(System::starting_point,
+		std::filesystem::directory_options::skip_permission_denied)) {
 		try {
+			// порівнюю ім'я файла (тут і директорії порівнюються але за умови
+			// шо юзер не дибіл і ввів екстеншн файла то все буде норм)
 			if (entry.path().filename() == file_name) {
+				// повертаю шлях файла назву якого ввів юзер
 				return entry.path();
 			}
 		}
+		// якшо шось іде по пизді то виводжу ексепшн
+		// але взагалі єдиний ексепшн цього коду наразі це якшо файл дуже далеко
+		// знаходиться на компі тоді там просто по часу програма не встигає
+		// його знайти. буду робити треди
 		catch (std::exception& ex) {
 			std::cerr << "Error: " << ex.what() << '\n';
 		}
 	}
+	// якшо програма не знайшла файл - виводиться пустий рядок
+	// треба буде зробити можливість шукати файл знову через do-while
 	return {};
 }
