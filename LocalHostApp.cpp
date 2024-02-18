ba76208ebdb8205db6cf80e4934127ce084509f8
Polina Cherkasova <polina_cherkasova@outlook.com>
2024-02-18 17:33:39 +0200

    added new comments

--- LocalHostApp.cpp
+++ LocalHostApp.cpp
@@ -1,14 +1,19 @@
 #include "System.h"
+// я не впевнена чи iostream тут треба бо я його уже включила в System.cpp
 #include <iostream>
 
 int main()
 {
+    // юзер інпут
     std::string input_file{};
     std::cout << "hello! enter file name here: ";
     std::cin >> input_file;
+    
+    // чищу буфер
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
+    // пошук самого файла
     const auto found_path = System::find_file_path(input_file);
     std::cout << found_path << '\n';
 
